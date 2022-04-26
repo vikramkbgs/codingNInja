de664ca45b7b4a388bf585bdbf006080bf48a17c
vikramkbgs@gmail.com <vikramkbgs@gmail.com>
2022-04-26 17:39:06 +0530

    charcter reverse pattern

---
+++ Web_Developer_Track/Introduction_to_java/interestingAlphabet.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+public class interestingAlphabet {
+    public static void main(String[] args) {
+        // Write your code here
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int k = 0;
+        s.close();
+        for (int i = 'A' + n; i >='A'; i--) {
+            for (int j = i; j <= i + k ; j++)
+                System.out.print((char)(j));
+            System.out.println();
+            k++;
+        }
+
+    }
+}
