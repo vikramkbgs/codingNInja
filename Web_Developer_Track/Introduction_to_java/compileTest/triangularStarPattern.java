308e128ad32542d01a1bf753634ce274bc68690f
vikramkbgs@gmail.com <vikramkbgs@gmail.com>
2022-04-26 15:45:18 +0530

    triangular star pattern

---
+++ Web_Developer_Track/Introduction_to_java/compileTest/triangularStarPattern.java
@@ -0,0 +1,16 @@
+// package NinjaCarrerCampApril.Web_Developer_Track.Introduction_to_java.compileTest;
+import java.util.*;
+public class triangularStarPattern {
+    public static void main(String[] args) {
+        // Write your code here
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        s.close();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+
+    }
+}
