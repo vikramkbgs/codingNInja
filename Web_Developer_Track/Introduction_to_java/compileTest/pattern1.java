ff7572dd73e4886499a9c69c8c72a6794ba2bed9
vikramkbgs@gmail.com <vikramkbgs@gmail.com>
2022-04-26 15:36:36 +0530

    first pattern of same number

---
+++ Web_Developer_Track/Introduction_to_java/compileTest/pattern1.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+
+public class pattern1{
+
+    public static void main(String[] args) {
+        // Write your code here
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        
+        for(int i =0; i<n; i++)
+          {  
+              for(int j =0; j<n; j++)
+                System.out.print(n);
+                System.out.println();
+        }
+
+    }
+}
