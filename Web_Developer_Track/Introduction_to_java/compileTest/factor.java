320230b86360534986acf06d380ef02866c06f79
vikramkbgs@gmail.com <vikramkbgs@gmail.com>
2022-04-26 15:20:52 +0530

    factor the given number

---
+++ Web_Developer_Track/Introduction_to_java/compileTest/factor.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+
+public class factor{
+
+    public static void main(String[] args) {
+        // Write your code here
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int i = 2;
+        while (i <= n/2) {
+
+            if(n % i == 0)
+            System.out.print(i+" ");
+            i++;
+        }
+        System.out.println(n);
+
+    }
+}
